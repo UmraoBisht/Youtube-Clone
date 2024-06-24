5d311f9cc657a7483ab2e7c9fe6b2c73239355cf
Umrao Bisht <umraobisht1@gmail.com>
2024-06-24 16:55:26 +0530

    add utils initial code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", () => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // (async () => {
 //   try {
